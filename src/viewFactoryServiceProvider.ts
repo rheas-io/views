629d109f06f1a814d0130e262a68530a36300710
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-09-04 20:51:12 +0530

    service resolver used instead of register

--- src/viewFactoryServiceProvider.ts
+++ src/viewFactoryServiceProvider.ts
@@ -1,15 +1,16 @@
 import { ViewFactory } from './viewFactory';
 import { DeferredServiceProvider } from '@rheas/services';
+import { InstanceHandler } from '@rheas/contracts/container';
 
 export class ViewFactoryServiceProvider extends DeferredServiceProvider {
     /**
      * Registers view factory on the application. This factory is responsible
      * for creating a view handler for each request. The container will be the
      * app instance.
+     *
+     * @returns
      */
-    public register() {
-        this.container.singleton(this.name, (app) => {
-            return new ViewFactory(app);
-        });
+    public serviceResolver(): InstanceHandler {
+        return (app) => new ViewFactory(app);
     }
 }
--- src/viewServiceProvider.ts
+++ src/viewServiceProvider.ts
@@ -1,17 +1,23 @@
 import { ViewFactory } from './viewFactory';
 import { app } from '@rheas/support/helpers';
 import { DeferredServiceProvider } from '@rheas/services';
+import { InstanceHandler } from '@rheas/contracts/container';
 
 export class ViewServiceProvider extends DeferredServiceProvider {
     /**
-     * Registers view handlers when a node receives a new request.
-     * The container will be the request instance.
+     * Returns view manager service of each request. This service is
+     * registered in the request lifecycle.
+     *
+     * App level view factory creates a view manager for the request
+     * lifecycle, so that app level settings and data can be referenced.
+     *
+     * @returns
      */
-    public register() {
-        this.container.singleton(this.name, (request) => {
+    public serviceResolver(): InstanceHandler {
+        return () => {
             const viewFactory: ViewFactory = app().get('view');
 
             return viewFactory.createNewView();
-        });
+        };
     }
 }
