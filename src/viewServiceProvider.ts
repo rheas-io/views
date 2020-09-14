9425a1d29ad1186650685e56709039c0d2601d4e
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-09-14 18:22:23 +0530

    renamed to view service provider as der is only 1

--- src/viewServiceProvider.ts
+++ src/viewServiceProvider.ts
@@ -2,7 +2,7 @@ import { ViewFactory } from './viewFactory';
 import { DeferredServiceProvider } from '@rheas/services';
 import { InstanceHandler } from '@rheas/contracts/container';
 
-export class ViewFactoryServiceProvider extends DeferredServiceProvider {
+export class ViewServiceProvider extends DeferredServiceProvider {
     /**
      * Registers view factory on the application. This factory is responsible
      * for creating a view handler for each request. The container will be the
